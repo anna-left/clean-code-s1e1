5b86d3ea398ab34a4f4cba148fc830f031140292
anna-left <anna_left@mail.ru>
2021-11-28 12:48:07 +0300

    fix: use BEM notation according to rule 2.1 in index.html,app.js

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ var createNewTaskElement=function(taskString){
     editInput.className = "list-item__text";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="list-item__edit";
 
     deleteButton.className="delete";
     deleteButtonImg.src='./remove.svg';
@@ -86,7 +86,7 @@ var editTask=function(){
 
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
-    var editBtn=listItem.querySelector(".edit");
+    var editBtn=listItem.querySelector(".list-item__edit");
     var containsClass=listItem.classList.contains("editMode");
     //If class of the parent is .editmode
     if(containsClass){
@@ -158,7 +158,7 @@ var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
     console.log("bind list item events");
 //select ListItems children
     var checkBox=taskListItem.querySelector("input[type=checkbox]");
-    var editButton=taskListItem.querySelector("button.edit");
+    var editButton = taskListItem.querySelector("button.list-item__edit");
     var deleteButton=taskListItem.querySelector("button.delete");
 
 
