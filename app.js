84575fa42db2dd0eb94aad316608525324dfa811
anna-left <anna_left@mail.ru>
2021-11-28 12:29:40 +0300

    fix: use BEM notation according to rule 2.1 in index.html, style.css

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ var createNewTaskElement=function(taskString){
 
     //input (checkbox)
     var checkBox=document.createElement("input");//checkbx
+    checkBox.classList.add("list-item__checkbox");
     //label
     var label=document.createElement("label");//label
     //input (text)
@@ -39,7 +40,7 @@ var createNewTaskElement=function(taskString){
     //Each elements, needs appending
     checkBox.type="checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className = "list-item__text";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
     editButton.className="edit";
